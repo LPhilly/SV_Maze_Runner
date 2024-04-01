b9245d3e2d4c16f2dfde42a083b41fa4776b2a07
Paras Kumar <90861109+ParasK02@users.noreply.github.com>
2024-04-01 15:16:39 -0400

    Added collision and player movement
    Added collision and player movement

--- Level3.sv
+++ Level3.sv
@@ -1,4 +1,4 @@
-module FinalLevel3 (
+module Level3 (
    // VGA-related signals
 pixel_clk,
    col,
@@ -8,14 +8,14 @@ pixel_clk,
    blue,
    // input push buttons and switches
    resetSwitch,
-   //add more inputs based on FSM
+   switches
    );
 
 input [9:0]  col;
 input [8:0]  row;
 
 input  resetSwitch,pixel_clk ;
-    
+input [3:0] switches;
 
 output logic [3:0]  red;    // 4-bit color output
 output logic [3:0]  green;  // 4-bit color output
@@ -23,7 +23,8 @@ output logic [3:0]  blue;   // 4-bit color output
 logic clk;
 
 FastClock(pixel_clk,clk);
-// Internal signals for rectangle properties
+logic withinAnyRectangle;
+
 
 //Rectangle 1
 logic [9:0] rect_x1 = 0;
@@ -50,7 +51,7 @@ logic [9:0] rect_width4 = 220;
 logic [9:0] rect_height4 = 35;
 
 //Rectangle 5(finish)
-logic [9:0] rect_x5 = 600;
+logic [9:0] rect_x5 = 570;
 logic [8:0] rect_y5 = 220;
 logic [9:0] rect_width5 = 50;
 logic [9:0] rect_height5 = 35;
@@ -61,14 +62,26 @@ logic [8:0] rect_y6 = 90;
 logic [9:0] rect_width6 = 50;
 logic [9:0] rect_height6 = 300;
 
+//Player
+logic [9:0] playerX = 13;
+logic [9:0] playery = 230;
+logic [9:0] playerWidth = 25;
+logic [9:0] playerHeight = 25;
 
 
 
 
 	
 always_ff begin
+		if (col >= playerX && col < playerX + playerWidth && row >= playery && row < playery + playerHeight)
+			begin 
+				red <= 4'hF; 
+				blue <= 4'hF;
+				green <= 0;
+			end 
+		
 		//Start Rectangle
-		if (col >= rect_x6 && col < rect_x6 + rect_width6 && row >= rect_y6 && row < rect_y6 + rect_height6)
+		else if (col >= rect_x6 && col < rect_x6 + rect_width6 && row >= rect_y6 && row < rect_y6 + rect_height6)
 			begin 
 				red <= 0; 
 				blue <= 0;
@@ -82,11 +95,6 @@ always_ff begin
 				green <= 4'hF;
 			end 
 			
-			//Start Rectangle
-		
-		
-
-
       //draw rectangle2
      else if (col >= rect_x2 && col < rect_x2 + rect_width2 && row >= rect_y2 && row < rect_y2 + rect_height2)
 			begin 
@@ -94,9 +102,6 @@ always_ff begin
 				blue <= 4'hF;
 				green <= 4'hF;
 			end 
-		
-
-
 
       //draw rectangle3
       else if (col >= rect_x3 && col < rect_x3 + rect_width3 && row >= rect_y3 && row < rect_y3 + rect_height3)
@@ -105,7 +110,13 @@ always_ff begin
 				blue <= 4'hF;
 				green <= 4'hF;
 			end 
-
+			//Finish Rectangle
+		else if (col >= rect_x5 && col < rect_x5 + rect_width5 && row >= rect_y5 && row < rect_y5 + rect_height5)
+			begin 
+				red <= 4'hF; 
+				blue <= 0;
+				green <= 0;
+			end 
 
 
 
@@ -117,13 +128,6 @@ always_ff begin
 				green <= 4'hF;
 			end 
 		
-		//Finish Rectangle
-		else if (col >= rect_x5 && col < rect_x5 + rect_width5 && row >= rect_y5 && row < rect_y5 + rect_height5)
-			begin 
-				red <= 4'hF; 
-				blue <= 0;
-				green <= 0;
-			end 
 		
 
 		else begin
@@ -131,6 +135,43 @@ always_ff begin
 			blue <= 0;
 			green <= 0;
 		end
+end
+always_ff begin
+if ((playerX  > rect_x1 && playerX + playerWidth< rect_x1 + rect_width1 && playery  > rect_y1 && playery + playerHeight < rect_y1 + rect_height1) ||
+        (playerX + playerWidth > rect_x2 && playerX < rect_x2 + rect_width2 && playery > rect_y2 && playery + playerHeight< rect_y2 + rect_height2) ||
+        (playerX + playerWidth > rect_x3 && playerX < rect_x3 + rect_width3 && playery > rect_y3 && playery + playerHeight < rect_y3 + rect_height3) ||
+        (playerX + playerWidth > rect_x4 && playerX < rect_x4 + rect_width4 && playery > rect_y4 && playery + playerHeight < rect_y4 + rect_height4)) 
+    begin
+        withinAnyRectangle = 1; // Player is within at least one rectangle
+    end
+	 else withinAnyRectangle = 0;
+	 
+	 
+end
+always @(posedge clk) begin
+
+	 
+	 if(!withinAnyRectangle)begin
+		playerX <= 13;
+		playery <= 230;
+		end
+
+	  //move left
+	  if (switches[3] == 1 && withinAnyRectangle) begin
+			playerX <= playerX - 5;
+	  end
+	  //move up
+	  else if (switches[2] == 1 && withinAnyRectangle ) begin
+			playery <= playery - 5;
+	  end
+	  //move down
+	  else if (switches[1] == 1 && withinAnyRectangle ) begin
+			playery <= playery + 5;
+	  end
+	  //move right
+	  else if (switches[0] == 1 && withinAnyRectangle) begin
+			playerX <= playerX + 5;
+	  end
 end
 
           
